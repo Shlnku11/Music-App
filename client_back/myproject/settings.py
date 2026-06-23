bc8992f139d63702a0c69b9cc5e9a09efca9d18c
Shlnku11~ <darkripy64@gmail.com>
2026-06-23 07:40:47 +0600

    fix: переключил базу данных на sqlite3 для деплоя

--- client_back/myproject/settings.py
+++ client_back/myproject/settings.py
@@ -84,19 +84,26 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 USE_POSTGRES = True
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+
+#         "NAME": os.getenv("DB_NAME"),
+
+#         "USER": os.getenv("DB_USER"),
+
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+
+#         "HOST": os.getenv("DB_HOST"),
+
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-
-        "NAME": os.getenv("DB_NAME"),
-
-        "USER": os.getenv("DB_USER"),
-
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-
-        "HOST": os.getenv("DB_HOST"),
-
-        "PORT": os.getenv("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
